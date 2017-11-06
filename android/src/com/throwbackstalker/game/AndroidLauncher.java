c660de4d53534b3e9018dd6fed64bf0ea1abaafc
mkazachkov <devmaksi@gmail.com>
2017-11-06 21:43:24 +0400

    first commit

---
+++ android/src/com/throwbackstalker/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.throwbackstalker.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.throwbackstalker.game.ThrowBackStalkerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ThrowBackStalkerGame(), config);
+	}
+}
---
+++ desktop/src/com/throwbackstalker/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.throwbackstalker.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.throwbackstalker.game.ThrowBackStalkerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ThrowBackStalkerGame(), config);
+	}
+}
